0b2495e0c6fa83d58efa2f46d3e4ec93a5c32071
Bdisha123 <bidishamo14012003@gmail.com>
2024-05-23 20:45:13 +0530

    write updated

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,67 +1,3 @@
-// "use client";
-
-// import React, { useState } from 'react'
-// import styles from "./writePage.module.css"
-// import Image from 'next/image'
-// import ReactQuill from "react-quill"
-// import "react-quill/dist/quill.bubble.css";
-// import { signIn, useSession } from 'next-auth/react';
-// import { useRouter } from 'next/navigation';
-
-// const WritePage = () => {
-//     const [open, setOpen] = useState(false)
-//     const [value, setValue] = useState("")
-
-//     const { status } = useSession()
-//     const router = useRouter();
-
-
-
-//     if (status === "loading") {
-//         return <div className={styles.loading}>Loading...</div>;
-//     }
-
-//     if (status === "unauthenticated") {
-//         router.push("/")
-//     }
-
-
-
-//     return (
-//         <div className={styles.container}>
-//             <input type="text" placeholder="Title" className={styles.input} />
-//             <div className={styles.editor}>
-//                 <button className={styles.button} onClick={() => setOpen(!open)}>
-//                     <Image src="/plus.png" alt='' width={16} height={16} />
-//                 </button>
-//                 {open && (
-//                     <div className={styles.add}>
-//                         <button className={styles.addButton}>
-//                             <Image src="/image.png" alt='' width={16} height={16} />
-//                         </button>
-//                         <button className={styles.addButton}>
-//                             <Image src="/external.png" alt='' width={16} height={16} />
-//                         </button>
-//                         <button className={styles.addButton}>
-//                             <Image src="/video.png" alt='' width={16} height={16} />
-//                         </button>
-//                     </div>
-//                 )}
-//                 <ReactQuill
-//                     className={styles.textArea}
-//                     theme="bubble"
-//                     value={value}
-//                     onChange={setValue}
-//                     placeholder="Tell your story..."
-//                 />
-//             </div>
-//             <button className={styles.publish}>Publish</button>
-//         </div>
-//     )
-// }
-
-// export default WritePage
-
 "use client";
 
 import Image from "next/image";
@@ -77,7 +13,10 @@ import {
     getDownloadURL,
 } from "firebase/storage";
 import { app } from "@/utils/firebase";
-import ReactQuill from "react-quill";
+// import ReactQuill from "react-quill";
+import dynamic from "next/dynamic";
+
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
 const WritePage = () => {
     const { status } = useSession();
@@ -216,3 +155,5 @@ const WritePage = () => {
 };
 
 export default WritePage;
+
+
